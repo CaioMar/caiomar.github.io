e0535f66ee61875f4c6406fa15645d9aecbf691b
CaioMar <caio.outmatched@gmail.com>
2021-10-28 12:24:16 -0300

    rearranging folders

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,26 @@
 import React from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import { robots } from "./robots";
-import './App.css'
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import './App.css';
+import Scroll from "../components/Scroll";
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         };
     }
 
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users => this.setState({ robots: users }));        
     }
 
     render() {
@@ -25,7 +31,9 @@ class App extends React.Component {
             <div className='tc'>
                 <h1 className='f1'>kittyfriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots} />
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
             </div>
         )
     }
